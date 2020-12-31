2e9508269a652981f987f4bea6acc199e2a9f4ba
Aliaksandr Senkevich <puskin1914@gmail.com>
2020-12-31 17:08:55 +0300

    feat add server with User model

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+const { ObjectID } = require("bson");
+const { Schema, model } = require("mongoose");
+
+const User = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  diskSpace: { type: Number, default: 1024 ** 3 * 10 },
+  usedSpace: { type: Number, default: 0 },
+  avatar: { type: String },
+  files: [{ type: ObjectID, ref: "File" }],
+});
+
+module.exports = model("User", User);
